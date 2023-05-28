5da24c980a2afbbb9f23c145df72a1257f7f15ee
taukirsheikh <taukirsheikh2@gmail.com>
2023-05-28 05:53:17 +0545

    json response updated

--- server/jobs/settings.py
+++ server/jobs/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scraper'
+    'scraper',
+    'corsheaders',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- server/scraper/views.py
+++ server/scraper/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .jobs_scrapper import scrape_jobs
 from django.views import View
+import json
 # Create your views here.
 
 class JobView(View):
     def get(self,request):
         position='python'
         records=scrape_jobs(position)
-        data={'records':records}
-        return JsonResponse(data)
+        data=json.dumps(records)
+        # return JsonResponse(data, safe=False)
+        return JsonResponse(records, safe=False)
