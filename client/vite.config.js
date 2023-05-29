1119702cdf2bf51bc5c872cef488cd89fdfba57c
taukirsheikh <taukirsheikh2@gmail.com>
2023-05-29 15:35:52 +0545

    config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,3 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+module.exports = {
+  build: {
+    rollupOptions: {
+      // Skip emitting error on import when using external packages
+      onwarn: (warning, rollupWarn) => {
+        if (warning.code !== 'MISSING_NAME') {
+          rollupWarn(warning);
+        }
+      },
+    },
+  },
+};
